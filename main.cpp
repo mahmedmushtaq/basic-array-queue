8a3f7aa87791e64d1cea0fcea5f890ec44934c2e
M Ahmed Mushtaq <mahmedmushtaq296@gmail.com>
2020-12-03 12:19:20 +0500

    Basic Queue

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+
+
+#define MAX_SIZE 2
+
+class Queue{
+  int front,rear;
+  string items[MAX_SIZE];
+  public:
+  Queue(){
+    front = -1;
+    rear = -1;
+  }
+  bool isEmpty(){
+    return front == -1 && rear == -1;
+  }
+  bool isFull(){
+    return (rear+1) % MAX_SIZE == front;
+  }
+
+  void enqueue(string item){
+    if(isFull()){
+      cout << "Queue is overflow";
+      return;
+    }else if (isEmpty()) {
+        front = rear = 0;
+    } else {
+        rear = (rear + 1) % MAX_SIZE;
+    }
+    
+    items[rear] = item;
+
+     
+  }
+
+  string dequeue(){
+    string dataToReturn = "";
+    
+    if (isEmpty()) {
+        cout << "Queue is underflow " << endl;
+    } else if (front == rear) {
+        // only one element.
+        dataToReturn = items[front];
+        front = rear = -1;
+    } else {
+        dataToReturn = items[front];
+        front = (front + 1) % MAX_SIZE;
+    }
+    
+    return dataToReturn;
+
+  }
+
+  void clear(){
+    front = rear = -1;
+  }
+
+  void firstElement(){
+    if(front == -1) return;
+    cout << items[front] << endl;
+  }
+
+ 
+};
+
+int main() {
+
+
+}
